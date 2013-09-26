2a075bf5171f9904c0ce5b703fc87b0a9d190cba
Takashi Kanemoto <t-kanemoto@quartet-mail.com>
2013-09-27 00:16:57 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,34 @@
-TreeHelper
-==========
+# TreeHelper
 
 CakePHP2 Helper to print list of TreeBehavior-model as nested &lt;ul> tag.
+
+## Requirement
+
+CakePHP >= 2.0
+
+## Usage
+
+### Model
+
+    class Category extends AppModel
+    {
+        public $actsAs = array('Tree');
+            :
+
+### Controller
+
+    class CategoriesController extends AppController
+    {
+        public $helpers = array('Html', 'Tree');
+
+        public function some_action()
+        {
+            $categories = $this->Category->find('all', array('order' => 'lft ASC'));
+            $this->set(compact('categories'));
+                :
+
+### View
+
+    echo $this->Tree->nestedUl($categories, 'Category', function ($node) {
+        return $this->Html->link($node['name'], array('action' => 'view', $node['id']));
+    });
---
+++ app/View/Helper/TreeHelper.php
@@ -0,0 +1,32 @@
+<?php
+App::uses('AppHelper', 'View/Helper');
+App::uses('Hash', 'Utility');
+
+class TreeHelper extends AppHelper
+{
+	public $helpers = array('Html');
+
+	/**
+	 * print list of TreeBehavior-model as nested ul tag.
+	 *
+	 * @param array $list array generated with "TreeBehaviorModel::find('all', array('order' => 'lft ASC'))".
+	 * @param string $modelName model name.
+	 * @param callable $html closure to convert one record to html.
+	 * @return string
+	 */
+	public function nestedUl(array $list, $modelName, callable $html)
+	{
+		$list = Hash::nest($list);
+
+		$out = '';
+		foreach ($list as $node) {
+			$children = '';
+			if (!empty($node['children'])) {
+				$children = $this->nestedUl($node['children'], $modelName, $html);
+			}
+			$out .= $this->Html->tag('li', $html($node[$modelName]) . $children);
+		}
+
+		return $out ? $this->Html->tag('ul', $out) : '';
+	}
+}
